0918513d6f13e3bea41e3125056b39c0aeb5db5f
Daniel MacDonald <daniel.macdonald@coyote.com>
2021-01-13 16:48:36 -0600

    Initial commit

---
+++ Api/Controllers/FeatureFlagsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement;
+
+namespace Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeatureFlagsController : ControllerBase
+    {
+        private readonly IFeatureManager _featureManager;
+
+        public FeatureFlagsController(IFeatureManager featureManager)
+        {
+            _featureManager = featureManager;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> IsEnabledAsync(string featureFlagName)
+        {
+            try
+            {
+                // Note: `IsEnabledAsync` will return `false` when no feature flag of the provided name is found
+                var enabled = await _featureManager.IsEnabledAsync(featureFlagName);
+
+                return Ok(enabled);
+            }
+            catch (SystemException)
+            {
+                return Ok(false);
+            }
+        }
+    }
+}
---
+++ Api/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) => {
+                    var settings = config.Build();
+                    config.AddAzureAppConfiguration(options => {
+                        options
+                            .Connect(settings["ConnectionStrings:AppConfig"])
+                            .UseFeatureFlags(featureFlagOptions =>
+                            {
+                                featureFlagOptions.CacheExpirationInterval = TimeSpan.FromSeconds(2);
+                            });
+                    });
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
